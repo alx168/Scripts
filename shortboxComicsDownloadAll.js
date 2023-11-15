ad61b2b696b28457bb4b72acdb6ca1136c8647e1
Alexander Guo <alx168@gmail.com>
2023-11-14 23:56:55 -0600

    Update shortboxComicsDownloadAll.js

--- shortboxComicsDownloadAll.js
+++ shortboxComicsDownloadAll.js
@@ -4,7 +4,7 @@ for(i = 0; i < listOfDownloads.childElementCount; i++) {
 }
 /*
 NOTE!!! 
- I'm annoyed that shortbox comics doesn't have a download all button for comic hoarders like me. 
+ I'm annoyed that shortbox comics fair doesn't have a download all button for comic hoarders like me. 
  This garbo code is quick and dirty and doesn't work for whatever reason in firefox. 
  In chrome and edge you will need to allow popups on your order's downloads page for this to download everything. 
  This note also serves as your epilepsy warning. 
